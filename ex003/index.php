a7565002a0badc6b7cbc7a9fa116f77d694d0354
Daniela Mota <danielafcmota.2006@gmail.com>
2024-05-12 17:17:49 +0100

    ex003

--- ex003/index.php
+++ ex003/index.php
@@ -10,6 +10,22 @@
 <body>
     <h1>Teste de tipos primitivos</h1>
     <?php 
+        // 0x -> hexadecimal; 0b -> binário; 0-> octal (TUDO INT)
+        $v = 33.4; 
+        var_dump($v);
+        
+        $n = "Daniela";
+        var_dump($n);
+
+        $vet = [2.5, 'daniela', 7, false];
+        var_dump($vet);
+
+        class Pessoa {
+            private string $nome;
+        }
+        
+        $p = new Pessoa;
+        var_dump($p)
         
     ?>
 </body>
