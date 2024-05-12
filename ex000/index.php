1b7ec42978d65c49183c712678740ccaea0941b0
Daniela Mota <danielafcmota.2006@gmail.com>
2024-05-12 16:56:38 +0100

    Initial commit

---
+++ ex000/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-pt">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>A livrar-me da maldição</title>
+</head>
+
+<body>
+    <h1>
+        <?php 
+            echo "Olá,  mundo! \u{1F30D}" # ou print
+        ?>
+    </h1>
+    <p>Vamos tentar livrar-nos da maldição</p>
+
+</body>
+
+</html>
